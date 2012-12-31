ddeee1a2e475a809b54806428c6195745e15db87
Tikhon Krasovitskiy <tik714@yandex.ru>
2012-12-31 14:53:39 +0300

    create the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyTravelling::Application.config.secret_token = 'b4334875be14e11c1f2f33af20f80e109252c1ce97e9b92c6c7c58ed5d1c28bb2dc37132120e9e4350d6e1c8bef31f2fbb73f921ef3061a0fb32e61db6c0967e'
